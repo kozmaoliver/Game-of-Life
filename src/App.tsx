ea26608d86b0233d4cf92fc4046b5b88e45edeab
oliver.kozma <oliver.kozma@trendency.hu>
2025-04-25 17:20:02 +0200

    Init Vite with Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-slate-900 text-white flex justify-center items-center">
+      <h1 className="text-4xl font-bold">Game of Life 🚀</h1>
+    </div>
+  );
+}
+
+export default App
